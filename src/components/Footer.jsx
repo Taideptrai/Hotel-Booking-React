31fe4b3ddf88108542e9865bee8c44e180daf1e7
Taideptrai <nguyenvantaizzz@gmail.com>
2021-02-05 15:22:22 +0200

    commit message

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { FaFacebookSquare,FaLinkedin } from 'react-icons/fa';
+import {IoLogoYoutube} from 'react-icons/io';
+import { AiFillInstagram } from 'react-icons/ai';
+
+const Footer = () => {
+    return (
+        <footer id="sticky-footer" className="bg-light text-dark-50">
+            <div className="container py-1">
+                <div className="row">
+                    <div className="col-md-6 col-12 my-auto">
+                    </div>
+                    <div className="col-md-6 col-12">
+                        <div className="d-flex float-right">
+                            
+                                <FaFacebookSquare className="connect text-dark" />
+                          
+                                <FaLinkedin className="connect text-dark" />
+                          
+                                <AiFillInstagram className="connect text-dark" />
+                          
+                                <IoLogoYoutube className="connect text-dark" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Footer
